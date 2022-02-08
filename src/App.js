bcb817a2b857a5a34e6f1a37da78a33688a206f2
Siarheispivak <siarheispivak2@gmail.com>
2022-02-08 17:54:11 +0300

    19 lesson

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import Header from './components/Header/Header';
 
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
-
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
 
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className={app - wrapper - content}>
+                    <Route path='/dialog' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
 
-      <Navbar />
+                </div>
 
-      <Profile />
-    </div>
-  );
+            </div>
+        </BrowserRouter>
+    );
 }
-
-
-
 
 
 export default App;
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './Dialog.module.css';
+
+const Dialogs = (props) => {
+    return(
+        <div>
+            Dialogs
+        </div>
+    )
+
+}
+
+export default  Dialogs;
